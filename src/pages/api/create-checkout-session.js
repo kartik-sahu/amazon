70a600df6fba511dbaf0f316857f3996e3ed9c1c
Kartik Sahu <59734886+kartik-sahu@users.noreply.github.com>
2021-06-09 19:41:52 +0530

    Update create-checkout-session.js

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -20,7 +20,6 @@ export default async (req, res) => {
   }));
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-    shipping_rates: ["shr_1IxWrzSHTm3ORKxpxEUvtggs"],
     shipping_address_collection: {
       allowed_countries: ["IN", "US", "CA"],
     },
